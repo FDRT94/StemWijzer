bf999161a50a2c741cccb8e4fa496e55fc0a79ea
Aron Marinelli <99048011@mydavinci.nl>
2019-02-15 11:04:50 +0100

    Add preference container (WIP)

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -1,6 +1,7 @@
 const introContainer = document.getElementById("intro-container")
 const questionContainer = document.getElementById("question-container")
 const resultContainer = document.getElementById("result-container")
+const preferenceContainer = document.getElementById("preference-container");
 const title = document.getElementById("title")
 const statement = document.getElementById("statement")
 
@@ -11,40 +12,33 @@ const contraBtn = document.getElementById("contra-btn")
 const resetBtn = document.getElementById("reset-btn")
 
 var answers = []
+var preferredStatements = []
 
 window.onload = function () {
     document.getElementById("intro-question-count").innerText = subjects.length
 
     document.getElementById("start-btn").onclick = function () {
-        questionContainer.classList.toggle("d-none")
-        introContainer.classList.toggle("d-none")
-        resetBtn.classList.toggle("d-none")
+        showContainer("question")
 
         loadQuestion(0);
     }
 
     document.getElementById("back-btn").onclick = function () {
         var index = parseInt(questionContainer.getAttribute("data-question-index"))
-        
+
         if (index == 0) {
-            questionContainer.classList.toggle("d-none")
-            introContainer.classList.toggle("d-none")
-            resetBtn.classList.toggle("d-none")
-            
+            showContainer("intro")
+
             sessionStorage.setItem("currentIndex", -1)
         } else {
             loadQuestion(index - 1)
         }
     }
-    
+
     resetBtn.onclick = function () {
         answers = []
 
-        questionContainer.classList.add("d-none")
-        resultContainer.classList.add("d-none")
-        resetBtn.classList.add("d-none")
-
-        introContainer.classList.toggle("d-none")
+        showContainer("intro")
     }
 
     proBtn.onclick = function () {
@@ -65,6 +59,10 @@ window.onload = function () {
         } else {
             calculateResult()
         }
+    }
+
+    document.getElementById("to-result-btn").onclick = function () {
+        calculateResult();
     }
 
     var currentIndex = sessionStorage.getItem("currentIndex")
@@ -122,7 +120,38 @@ function answerAndContinue(position) {
     if (index < (subjects.length - 1)) {
         loadQuestion(index + 1)
     } else {
-        calculateResult()
+        showPreferenceContainer()
+    }
+}
+
+function showPreferenceContainer() {
+    showContainer("preference")
+    const preferredStatementContainer = document.getElementById("preferred-statements");
+
+    preferredStatementContainer.innerHTML = "";
+    preferredStatements = [];
+
+    for(let i = 0; i < answers.length; i++) {
+        var answer = answers[i];
+        var subject = subjects[answer.index]
+
+        preferredStatementContainer.innerHTML += '<div class="form-check form-check-inline p-2"><label class="form-check-label"><input class="form-check-input preferred-statement-checkbox" type="checkbox" value="' + answer.index + '">' + subject.title + '</label></div>'
+    }
+
+    const checkboxes = document.getElementsByClassName("preferred-statement-checkbox");
+
+    for(let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].onclick = function() {
+            if (this.checked) {
+                preferredStatements.push(this.value)
+            } else {
+                var index = preferredStatements.indexOf(this.value);
+ 
+                if (index > -1) {
+                    arr.splice(index, 1);
+                }
+            }
+        }
     }
 }
 
@@ -159,40 +188,61 @@ function showResult() {
     const second = document.getElementById("second")
     const third = document.getElementById("third")
     const ranking = document.getElementById("ranking")
-
+    
     first.innerText = parties[0].name + " - " + parties[0].percentage + "%"
     second.innerText = parties[1].name + " - " + parties[1].percentage + "%"
     third.innerText = parties[2].name + " - " + parties[2].percentage + "%"
-
+    
+    ranking.innerHTML = "";
     for (let i = 3; i < parties.length; i++) {
         ranking.innerHTML += '<li class="list-group-item bg-light"><img src="Assets/Images/Parties/' + parties[i].name.replace(/\s+/g, '') + '.png"></img>' + parties[i].name + ' - ' + parties[i].percentage + '%</li>'
     }
 
-    resultContainer.classList.toggle("d-none")
-    questionContainer.classList.toggle("d-none")
+    showContainer("result")
 }
 
 function setSelectedAnswer(position) {
+    proBtn.classList.replace("btn-primary", "btn-secondary")
+    ambivalentBtn.classList.replace("btn-primary", "btn-secondary")
+    contraBtn.classList.replace("btn-primary", "btn-secondary")
+
     switch (position) {
         case "pro":
             proBtn.classList.replace("btn-secondary", "btn-primary")
-            ambivalentBtn.classList.replace("btn-primary", "btn-secondary")
-            contraBtn.classList.replace("btn-primary", "btn-secondary")
             break;
         case "ambivalent":
-            proBtn.classList.replace("btn-primary", "btn-secondary")
             ambivalentBtn.classList.replace("btn-secondary", "btn-primary")
-            contraBtn.classList.replace("btn-primary", "btn-secondary")
             break;
         case "contra":
-            proBtn.classList.replace("btn-primary", "btn-secondary")
-            ambivalentBtn.classList.replace("btn-primary", "btn-secondary")
             contraBtn.classList.replace("btn-secondary", "btn-primary")
             break;
         default:
-            proBtn.classList.replace("btn-secondary", "btn-primary")
-            ambivalentBtn.classList.replace("btn-secondary", "btn-primary")
-            contraBtn.classList.replace("btn-secondary", "btn-primary")
+            break;
+    }
+}
+
+function showContainer(container) {
+    introContainer.classList.add("d-none")
+    questionContainer.classList.add("d-none")
+    preferenceContainer.classList.add("d-none")
+    resultContainer.classList.add("d-none")
+    resetBtn.classList.add("d-none")
+
+    switch (container) {
+        case "intro":
+            introContainer.classList.remove("d-none")
+            break;
+        case "question":
+            questionContainer.classList.remove("d-none")
+            resetBtn.classList.remove("d-none")
+            break;
+        case "preference":
+            preferenceContainer.classList.remove("d-none")
+            resetBtn.classList.remove("d-none")
+            break;
+        case "result":
+            resultContainer.classList.remove("d-none")
+            resetBtn.classList.remove("d-none")
             break;
     }
 }
